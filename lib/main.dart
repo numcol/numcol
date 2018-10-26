ad31a0cff97579c36c29b495d900cbd8ec9c3915
Alberto Varela <alberto@berriart.com>
2018-10-26 19:15:26 +0200

    Adding remote config

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import 'dart:async';
+
+import 'package:flutter_crashlytics/flutter_crashlytics.dart';
 import 'package:audioplayers/audio_cache.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -26,35 +30,76 @@ import 'domain/index.dart';
 import 'view/index.dart';
 
 void main() async {
-  const zenModePoints = 10;
   final sharedPreferences = await SharedPreferences.getInstance();
-  final audioCache = AudioCache(prefix: 'audio/');
-  final storage = Storage(sharedPreferences);
-  final animatorFactory = AnimatorFactory();
-  final sharer = Sharer();
-  final analytics = FirebaseAnalytics();
-  final audio = AudioPlayer(storage, audioCache);
-  final game = Game(zenModePoints);
 
-  runApp(
-    Configuration(
-      initialTimeInMilliseconds: 10000,
-      timeReducerMultiplier: 0.99,
-      timePenaltyMultiplier: 0.66,
-      timeAdditionByAnswerInMilliseconds: 1800,
-      gameStartCountdownSeconds: 4,
-      zenModePoints: zenModePoints,
-      child: Injector(
-        dependencies: [
-          storage,
-          animatorFactory,
-          audio,
-          sharer,
-          analytics,
-          game,
-        ],
-        child: NumcolApp(),
-      ),
-    ),
-  );
+  var isDebugMode = false;
+  assert(() {
+    isDebugMode = true;
+    return true;
+  }());
+
+  FlutterError.onError = (FlutterErrorDetails details) {
+    if (isDebugMode) {
+      FlutterError.dumpErrorToConsole(details);
+    } else {
+      Zone.current.handleUncaughtError(details.exception, details.stack);
+    }
+  };
+
+  await FlutterCrashlytics().initialize();
+
+  runZoned<Future<Null>>(() async {
+    runApp(FutureBuilder<RemoteConfig>(
+      future: setupRemoteConfig(isDebugMode),
+      builder: (BuildContext context, AsyncSnapshot<RemoteConfig> snapshot) {
+        final audioCache = AudioCache(prefix: 'audio/');
+        final storage = Storage(sharedPreferences);
+        final remoteConfig = snapshot.data;
+
+        return snapshot.hasData
+            ? Configuration(
+                initialTimeInMilliseconds:
+                    remoteConfig.getInt('initialTimeInMilliseconds'),
+                timeReducerMultiplier:
+                    remoteConfig.getDouble('timeReducerMultiplier'),
+                timePenaltyMultiplier:
+                    remoteConfig.getDouble('timePenaltyMultiplier'),
+                timeAdditionByAnswerInMilliseconds:
+                    remoteConfig.getInt('timeAdditionByAnswerInMilliseconds'),
+                gameStartCountdownSeconds:
+                    remoteConfig.getInt('gameStartCountdownSeconds'),
+                zenModePoints: remoteConfig.getInt('zenModePoints'),
+                child: Injector(
+                  dependencies: [
+                    storage,
+                    AnimatorFactory(),
+                    AudioPlayer(storage, audioCache),
+                    Sharer(),
+                    FirebaseAnalytics(),
+                    Game(remoteConfig.getInt('zenModePoints')),
+                  ],
+                  child: NumcolApp(),
+                ),
+              )
+            : Container();
+      },
+    ));
+  }, onError: (error, stackTrace) async {
+    await FlutterCrashlytics()
+        .reportCrash(error, stackTrace, forceCrash: false);
+  });
+}
+
+Future<RemoteConfig> setupRemoteConfig(bool isDebugMode) async {
+  final RemoteConfig remoteConfig = await RemoteConfig.instance;
+  remoteConfig.setConfigSettings(RemoteConfigSettings(debugMode: isDebugMode));
+  remoteConfig.setDefaults(<String, dynamic>{
+    'initialTimeInMilliseconds': 10000,
+    'timeReducerMultiplier': 0.99,
+    'timePenaltyMultiplier': 0.66,
+    'timeAdditionByAnswerInMilliseconds': 1800,
+    'gameStartCountdownSeconds': 4,
+    'zenModePoints': 10,
+  });
+  return remoteConfig;
 }
